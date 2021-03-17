efad457b57a9aa7db3bff58576dfa48b9bc2c510
Skyler Shu <51185496+skylershu@users.noreply.github.com>
2021-03-17 11:09:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,16 +1,16 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
-  email: 'i@craigary.net',
-  link: 'https://nobelium.vercel.app',
-  description: 'This gonna be an awesome website.',
+  title: 'SkylerShu',
+  author: 'SkylerShu',
+  email: 'me@skylershu.com',
+  link: 'https://nobelium.skylershu.com',
+  description: 'Nobelium docs by SkylerShu',
   lang: 'en-US',
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // if leave this empty, current year will be used.
   postsPerPage: 7,
   showAbout: true, // WIP
   showArchive: true, // WIP
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://twitter.com/realSkylerShu',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
